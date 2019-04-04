7a79498f248206f7a81335ef3db49741aa7cd0bc
conorj <conorj@iastate.edu>
2019-04-04 13:01:49 -0500

    renames CombatUtil in order to reflect all it does, adds a method to find locations a character can move

--- src/charutil/CharacterAndBoardUtil.java
+++ src/charutil/CharacterAndBoardUtil.java
@@ -1,10 +1,11 @@
 package charutil;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import GameBoard.Board.Square;
 import character.BaseCharacter;
 
-public class CombatUtil {
+public class CharacterAndBoardUtil {
 	
 	/**
 	 * Gives list of all locations the character at x,y can attack
@@ -77,5 +78,43 @@ public class CombatUtil {
 		if(defender.getAC() <= toHit)
 			defender.takeDamage(attacker.attack());
 		return false;
+	}
+	
+	public HashSet<Pair> moveList(int x, int y, Square[][] board)
+	{
+		BaseCharacter c = board[x][y].getCharacter();
+		int mv = c.getMove();
+		return recursiveMoveList(x, y, board, mv);
+	}
+	
+	//this is pretty naive, will rework - conor
+	private HashSet<Pair> recursiveMoveList(int x, int y, Square[][] board, int d)
+	{
+		HashSet<Pair> moves = new HashSet<Pair>();
+		if(x >= 0 && y >= 0 && x < board.length && y < board[board.length].length)
+		{
+			moves.add(new Pair(x,y));
+			if( d > 0)
+			{
+				//top left
+				moves.addAll(recursiveMoveList(x-1, y-1, board, d-1));
+				//top right
+				moves.addAll(recursiveMoveList(x+1, y-1, board, d-1));
+				//above
+				moves.addAll(recursiveMoveList(x, y-1, board, d-1));
+				//left
+				moves.addAll(recursiveMoveList(x-1, y, board, d-1));
+				//right
+				moves.addAll(recursiveMoveList(x+1, y, board, d-1));
+				//bottom left
+				moves.addAll(recursiveMoveList(x-1, y+1, board, d-1));
+				//below
+				moves.addAll(recursiveMoveList(x, y+1, board, d-1));
+				//bottom right
+				moves.addAll(recursiveMoveList(x+1, y+1, board, d-1));
+			}
+				
+		}
+		return moves;
 	}
 }

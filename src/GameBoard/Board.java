8c047296a209b294ce7d2e165e5afb1f0f51e1bc
sammassey <samassey@iastate.edu>
2019-04-03 21:37:03 -0500

    Got rid of the whole X.jpg thing and created an actual board

--- src/GameBoard/Board.java
+++ src/GameBoard/Board.java
@@ -3,6 +3,7 @@ package GameBoard;
 import Multiplayer.MultiplayerHandler;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -51,17 +52,24 @@ public class Board extends Application {
 		Scene scene = new Scene(border, 1500, 1000);
 
 		restart = new Button("Restart Game");
+		restart.setPadding(new Insets(10, 10, 10, 10));
 		restart.setOnAction(e -> restart());
 
 		int row, column;
 
 		for (row = 0; row < 10; row++) {
 			for (column = 0; column < 15; column++) {
-				game.add(box[row][column] = new Square(row, column), column, row);
+				if(column == 7)
+				{
+					game.add(box[row][column] = new Square(row, column, column), column, row);
+				}
+				else
+				{
+					game.add(box[row][column] = new Square(row, column, column), column, row);
+				}
 			}
 		}
-
-		border.setTop(restart);
+		border.setRight(restart);
 		border.setCenter(game);
 
 		stage.setTitle("Temp");
@@ -76,6 +84,7 @@ public class Board extends Application {
 			}
 
 		});
+		
 	}
 
 	private void restart() {
@@ -90,43 +99,49 @@ public class Board extends Application {
 	}
 
 	public class Square extends StackPane {
-		private Image xImg;
-		private ImageView x;
 		private int xloc, yloc;
 
 		// Used to create and update the board
-		public Square(int xloc, int yloc) {
+		public Square(int xloc, int yloc, int column) {
 			this.xloc = xloc;
 			this.yloc = yloc;
+
+			
+			Image grassImg = new Image(getClass().getResource("grassTile.jpg").toExternalForm());
+			ImageView grass = new ImageView(grassImg);
+			
+			
+			Image pathImg = new Image(getClass().getResource("pathTile.jpg").toExternalForm());
+			ImageView path = new ImageView(pathImg);
+			
 			// Background color is white, boarders are black
-			this.setStyle("-fx-border-color: black;-fx-background-color: white;");
+			this.setStyle("-fx-border-color: black;");
 			this.setPrefSize(100, 100);
 			this.setOnMouseClicked(e -> mouseEvent());
 
-			// The X players markers
-			xImg = new Image(getClass().getResource("x.jpg").toExternalForm());
-
-			x = new ImageView(xImg);
-
-			x.setFitWidth(66);
-			x.setPreserveRatio(true);
-
-			this.getChildren().addAll(x);
-			x.setVisible(false);
+			grass.setFitWidth(99);
+			grass.setPreserveRatio(true);
+			
+			path.setFitWidth(99);
+			path.setPreserveRatio(true);
+			
+			if(column != 7)
+			{
+				this.getChildren().add(grass);
+			}
+			else
+			{
+				this.getChildren().add(path);
+			}
 		}
 
 		// Gets mouse event
 		public void mouseEvent() {
-			if (!x.isVisible()) {
-				x.setVisible(true);
-				if(connected) connection.createX(xloc, yloc);
-			} else {
-
-			}
+			if(connected) connection.createX(xloc, yloc);
 		}
 
 		public void setState() {
-			x.setVisible(false);
+
 		}
 
 		public int getX() {
